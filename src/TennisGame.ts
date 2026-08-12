6f323068fb9871197eae8db58f85779c184e1f83
Aditya Sah <asah@kyruus.com>
2026-08-12 17:25:02 +0530

    refactor: extract the small methods

--- src/TennisGame.ts
+++ src/TennisGame.ts
@@ -26,18 +26,45 @@ export class TennisGame1 implements TennisGame {
     }
 
     getScore(): string {
-        if(this.m_score1 === this.m_score2){
-            if(this.m_score1 >= 3) return 'Deuce';
-            return `${TENNIS_SCORE[this.m_score1]}-All`
+        if(this.isScoreEqual()){
+            return this.calculateEqualScore();
         }
-        if(this.m_score1 >= 4 || this.m_score2 >= 4) {
-            let minusResult: number = this.m_score1 - this.m_score2;
-            const player = minusResult > 0 ? this.player1Name : this.player2Name;
-            if(minusResult < 0 ) minusResult = -minusResult;
-            return minusResult === 1 ? `Advantage ${player}` : `Win for ${player}`;
+        if(this.isScoreGreaterThanThree()) {
+            let minusResult: number = this.getMinusResult();
+            const player = this.getPlayer(minusResult);
+            minusResult = Math.abs(minusResult);
+            return this.calculateScoreWhenOnePlayerHaveScore4(minusResult, player);
         }
-        return `${TENNIS_SCORE[this.m_score1]}-${TENNIS_SCORE[this.m_score2]}`;
+        return this.calculateScoreForDefaultCase();
     }
 
 
+    private calculateEqualScore() {
+        if (this.m_score1 >= 3) return 'Deuce';
+        return `${TENNIS_SCORE[this.m_score1]}-All`
+    }
+
+    private calculateScoreForDefaultCase() {
+        return `${TENNIS_SCORE[this.m_score1]}-${TENNIS_SCORE[this.m_score2]}`;
+    }
+
+    private isScoreGreaterThanThree() {
+        return this.m_score1 >= 4 || this.m_score2 >= 4;
+    }
+
+    private isScoreEqual() {
+        return this.m_score1 === this.m_score2;
+    }
+
+    private calculateScoreWhenOnePlayerHaveScore4(minusResult: number, player: string) {
+        return minusResult === 1 ? `Advantage ${player}` : `Win for ${player}`;
+    }
+
+    private getPlayer(minusResult: number) {
+        return minusResult > 0 ? this.player1Name : this.player2Name;
+    }
+
+    private getMinusResult() {
+        return this.m_score1 - this.m_score2;
+    }
 }
