38ca14a00107ff75a3ce486f3b1fbe70bbe73ec2
Aditya Sah <asah@kyruus.com>
2026-08-30 21:27:07 +0530

    refactor: made the playername to readonly

--- src/TennisGame.ts
+++ src/TennisGame.ts
@@ -10,8 +10,8 @@ const TENNIS_SCORE: Record<number, string> = {
 export class TennisGame1 implements TennisGame {
     private m_score1: number = 0;
     private m_score2: number = 0;
-    private player1Name: string;
-    private player2Name: string;
+    private readonly player1Name: string;
+    private readonly player2Name: string;
 
     constructor(player1Name: string, player2Name: string) {
         this.player1Name = player1Name;
